e268f97a955154d2009f75713fadab88ec1dac17
Rushabh025 <rushabh.mahawarkar@gmail.com>
2025-04-15 22:27:09 +0530

    Created Ejs page and js file with css

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
+const __dirname = path.resolve();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('pad')
+});
 
 app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
